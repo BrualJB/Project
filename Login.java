c46bd9fafb1cb1d4e52c4ea01aafa2392e588369
Emman <111825090+BrualJB@users.noreply.github.com>
2023-05-25 16:44:54 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,15 +1,18 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.JButton;
+import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Login {
+public class Login extends JFrame {
 
-	private JFrame LoginFrame;
+	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
 
@@ -20,8 +23,8 @@ public class Login {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login window = new Login();
-					window.LoginFrame.setVisible(true);
+					Login frame = new Login();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -30,62 +33,55 @@ public class Login {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public Login() {
-		initialize();
-	}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		LoginFrame = new JFrame();
-		LoginFrame.setBounds(100, 100, 450, 300);
-		LoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		LoginFrame.getContentPane().setLayout(null);
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(174, 11, 46, 14);
-		LoginFrame.getContentPane().add(lblNewLabel);
-		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setBounds(164, 163, 89, 23);
-		LoginFrame.getContentPane().add(btnNewButton);
-		
-		JButton btnRegister = new JButton("Register");
-		btnRegister.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Register1 RE = new Register1();
-				RE.setVisible(true);
-				dispose();
-
-			}
-		});
-		btnRegister.setBounds(164, 197, 89, 23);
-		LoginFrame.getContentPane().add(btnRegister);
+		JLabel lblNewLabel = new JLabel("NC PROJECTS LOGIN");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(125, 23, 160, 14);
+		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(134, 90, 152, 20);
-		LoginFrame.getContentPane().add(textField);
+		textField.setBounds(136, 126, 160, 20);
+		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(134, 121, 152, 20);
-		LoginFrame.getContentPane().add(textField_1);
+		textField_1.setBounds(136, 155, 160, 20);
+		contentPane.add(textField_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Username:");
-		lblNewLabel_1.setBounds(70, 93, 58, 14);
-		LoginFrame.getContentPane().add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(48, 127, 89, 14);
+		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Password:");
-		lblNewLabel_1_1.setBounds(70, 118, 58, 14);
-		LoginFrame.getContentPane().add(lblNewLabel_1_1);
-	}
-
-	protected void dispose() {
-		// TODO Auto-generated method stub
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1_1.setBounds(48, 156, 89, 14);
+		contentPane.add(lblNewLabel_1_1);
 		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(159, 186, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Register R = new Register();
+				R.setVisible(true);
+				dispose();
+			}
+		});
+		btnRegister.setBounds(159, 220, 89, 23);
+		contentPane.add(btnRegister);
 	}
 }
